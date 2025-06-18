35d8dd600578c87448ad0e0fc6e049a6083ce3ed
Moonlight <74971359+chandraprakash-demo@users.noreply.github.com>
2025-06-19 01:41:12 +0530

    Implementing Runnable interface using Lambda

--- Test.java
+++ Test.java
@@ -4,5 +4,18 @@ public static void main(String args[])
 {
 System.out.println("Welcome to core java.");
 System.out.println("Java is really nice.");
+Runnable r = ()->{
+  for(int i=1;i<=10;i++)
+    {
+      System.out.println("Java Session : "+i);
+      try
+        {
+          Thread.sleep(300);
+        }
+      catch(InterruptedException ie){}
+    }
+};
+  Thread t1 = new Thread(r);
+  t1.start();
 }
 }
