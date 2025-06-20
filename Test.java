fa0b24a14e2257c44e1ae654bab48089696df2b5
moonlight <java9light@gmail.com>
2025-06-20 09:03:25 +0530

    Fully modify uat branch.

--- Test.java
+++ Test.java
@@ -2,20 +2,6 @@ public class Test
 {
 public static void main(String args[])
 {
-System.out.println("Welcome to core java.");
-System.out.println("Java is really nice.");
-Runnable r = ()->{
-  for(int i=1;i<=10;i++)
-    {
-      System.out.println("Java Session : "+i);
-      try
-        {
-          Thread.sleep(300);
-        }
-      catch(InterruptedException ie){}
-    }
-};
-  Thread t1 = new Thread(r);
-  t1.start();
+System.out.println("Welcome to core Java.");
 }
 }
